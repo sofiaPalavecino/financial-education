d9ae7ba765c4918976d2f48facb4990d916055f5
sofiaPalavecino <sofiaor29@gmail.com>
2025-03-29 00:27:04 -0300

    added base styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import './App.scss'
 function App() {
 
   return (
-    <div>
-      <h1>Hola</h1>
-    </div>
+    <main>
+      <section>
+        <div className="wrap">
+          <h1>Hola</h1>
+        </div>
+      </section>
+    </main>
   )
 }
 
