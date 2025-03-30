3b01c07833320ef3eb5650db601e9cf5435cf979
CaroHasbani <hasbanicarolina@gmail.com>
2025-03-30 06:16:00 -0300

    fix styles

--- src/components/__mocks__/mockGoals.tsx
+++ src/components/__mocks__/mockGoals.tsx
@@ -3,7 +3,7 @@ import { IGoals } from "@/interfaces/IGoals"
 const mockGoals: IGoals[] = [
     {
         id: 1,
-        title: "Objetivo 1",
+        title: "Objetivo ejemplo",
         target_amount: 100,
         start_date: new Date("29/03/2025"),
         end_date: new Date("29/04/2025"),
